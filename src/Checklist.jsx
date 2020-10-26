498bb9cf8f957d602c85b0602cd5ad99072798f3
vkolencik <vojtech.kolencik@vistaprint.com>
2020-10-26 07:58:08 +0100

    Add prop types

--- src/Checklist.jsx
+++ src/Checklist.jsx
@@ -1,5 +1,6 @@
 // noinspection ES6UnusedImports
 import React, { h, Fragment } from 'preact'
+import PropTypes from 'prop-types'
 
 export const Checklist = ({ checklistData }) => (
   <Fragment>
@@ -13,3 +14,13 @@ export const Checklist = ({ checklistData }) => (
     </fieldset>
   </Fragment>
 )
+
+Checklist.propTypes = {
+  checklistData: PropTypes.shape({
+    heading: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    tasks: PropTypes.arrayOf(PropTypes.shape({
+      description: PropTypes.string.isRequired
+    })).isRequired
+  })
+}
